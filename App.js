fd1cda2ca6b7a6b5cffac5096bc6d67071e887b9
Steven Truong <s.ttruong@yahoo.com>
2023-04-12 22:22:37 -0400

    changes to login screen

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, TextInput, SafeAreaView, Alert, View} from 'react-native';
 
 export default function App() {
+  const [text, onChangeText] = React.useState('');
+  const [number, onChangeNumber] = React.useState('');
 
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Name"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Game Code"
+        keyboardType="numeric"
+      />
+
+    <View style={styles.button}>
+      <Button
+        title="OK"
+        color="black"
+        onPress={() => Alert.alert('Button pressed')}
+      />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+    //alignContent: "center",
   },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+
+  button: {
+    borderWidth: 1,
+    marginTop: 10,
+    padding: 5,
+    width: '40%',
+    position: 'relative',
+    //flexWrap: 'wrap',
+    backgroundColor: 'white',
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
 });
